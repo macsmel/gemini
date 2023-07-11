146a6efdbfdc7ceb5145f4a087da046c5c5d2ba5
Maxim Melnic <macsmel@yahoo.com>
2023-07-11 16:48:33 +0300

    -hashing integration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 "use strict";
 const FilesController = require("./controllers/filesController");
 const filesController = new FilesController();
+const Confirm = require('prompt-confirm');
 
 (async () => {
-  const length = await filesController.findDuplicates("FOLDER_PATH",
-    "SUB_FOLDER_DELETION_PATH_OR_REPEAT_FOLDER_PATH");
-  console.log(`Found: ${length} duplicates.`);
-  length > 0 && await filesController.deleteDuplicates();
+  new Confirm("Use Hash Mode?")
+    .ask(async (answer) => {
+      const length = await filesController.findDuplicates("FOLDER_PATH",
+        "SUB_FOLDER_DELETION_PATH_OR_REPEAT_FOLDER_PATH", answer);
+      console.log(`Found: ${length} duplicates.`);
+      length > 0 && await filesController.deleteDuplicates();
+    });
 })();
--- src/controllers/filesController.js
+++ src/controllers/filesController.js
@@ -1,32 +1,57 @@
 const fs = require("fs").promises;
+const { readFileSync} = require("fs");
 const { unlink } = fs;
 const path = require("path");
 const Confirm = require('prompt-confirm');
+const crypto = require("crypto");
 
 module.exports = class FilesController {
   #duplicates = [];
   #filenames = {};
+  #fileHashes = {};
   #skipped = [];
-  findDuplicates = async (dir, deletionDir) => {
+  #deletionDir = "";
+  #saveDuplicates = (filePath, savedFile) => {
+    this.#duplicates.push(filePath.includes(this.#deletionDir) ? filePath : savedFile.path);
+  };
+  #simpleComparison = ({stats, filePath, filename}) => {
+    const savedFile = this.#filenames[filename];
+    if (savedFile) {
+      if (savedFile.size === stats.size) {
+        this.#saveDuplicates(filePath, savedFile);
+      } else {
+        this.#skipped.push({a: filePath, b: savedFile.path, sizes: [savedFile.size, stats.size]});
+      }
+    } else {
+      this.#filenames[filename] = {...stats, path: filePath};
+    }
+  };
+  #hashComparison = ({filePath, stats}) => {
+    const fileData = readFileSync(filePath);
+    const fileHash = crypto.createHash("md5").update(fileData).digest("hex");
+    const savedFile = this.#fileHashes[fileHash];
+    if (savedFile) {
+      this.#saveDuplicates(filePath, savedFile);
+    } else {
+      this.#fileHashes[fileHash] = {...stats, path: filePath};
+    }
+  };
+  #outputList = async (name, list) => {
+    await fs.writeFile(name, JSON.stringify(list,null,2));
+  };
+  findDuplicates = async (dir, deletionDir, hashMode) => {
     const files = await fs.readdir(dir);
+    this.#deletionDir = deletionDir;
     for (const file of files) {
       if (file.startsWith(".")) continue;
       const filePath = path.join(dir, file);
       const filename = path.parse(file).base;
       const stats = await fs.stat(filePath);
       if (stats.isDirectory()) {
-        await this.findDuplicates(filePath, deletionDir);
+        await this.findDuplicates(filePath, deletionDir, hashMode);
       } else {
-        const savedFile = this.#filenames[filename];
-        if (savedFile) {
-          if (savedFile.size === stats.size) {
-            this.#duplicates.push(filePath.includes(deletionDir) ? filePath : savedFile.path);
-          } else {
-            this.#skipped.push({a: filePath, b: savedFile.path, sizes: [savedFile.size, stats.size]});
-          }
-        } else {
-          this.#filenames[filename] = {...stats, path: filePath};
-        }
+        const info = { filePath, stats, filename };
+        hashMode ? this.#hashComparison(info) : this.#simpleComparison(info);
       }
     }
     this.#duplicates = this.#duplicates.filter(path => path.includes(deletionDir));
@@ -40,8 +65,8 @@ module.exports = class FilesController {
             await Promise.all(this.#duplicates.map(path => unlink(path)));
             console.log("Done.");
           } else {
-            await fs.writeFile("duplicates.json", JSON.stringify(this.#duplicates,null,2));
-            await fs.writeFile("skipped.json", JSON.stringify(this.#skipped,null,2));
+            await this.#outputList("duplicates.json", this.#duplicates);
+            await this.#outputList("skipped.json", this.#skipped);
             console.log("File duplicates.json and skipped.json were created.");
           }
         } catch (err) {
